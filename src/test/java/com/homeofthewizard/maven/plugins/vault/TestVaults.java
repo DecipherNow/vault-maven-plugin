7595e497773ab527ed54310384429f8327651108
Ozgun OZ <ozgunn.oz@gmail.com>
2023-03-31 00:08:22 +0200

    add tests for pull and push mojos

--- src/test/java/com/homeofthewizard/maven/plugins/vault/TestVaults.java
+++ src/test/java/com/homeofthewizard/maven/plugins/vault/TestVaults.java
@@ -2,8 +2,11 @@ package com.homeofthewizard.maven.plugins.vault;
 
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.api.Logical;
+import com.bettercloud.vault.response.LogicalResponse;
 import com.homeofthewizard.maven.plugins.vault.client.VaultBackendProvider;
 import com.homeofthewizard.maven.plugins.vault.config.AuthenticationMethodFactory;
+import com.homeofthewizard.maven.plugins.vault.config.Path;
 import com.homeofthewizard.maven.plugins.vault.config.Server;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,7 +15,9 @@ import org.mockito.Mockito;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import static com.homeofthewizard.maven.plugins.vault.VaultTestHelper.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -58,7 +63,19 @@ public class TestVaults {
     }
 
     @Test
-    public void testPull() throws VaultException {
+    public void testPushSkip() throws VaultException {
+        var server = new Server("URL", null, false, null, null, "NAMESPACE", List.of(), true, 1);
+        var vaultBackendProviderMock = Mockito.mock(VaultBackendProvider.class);
+        when(vaultBackendProviderMock.vault(any(),any(),any(),anyBoolean(),any(),any())).thenReturn(null);
+        var vaultClient = Vaults.createForBackend(vaultBackendProviderMock);
+
+        vaultClient.push(List.of(server), null);
+
+        verify(vaultBackendProviderMock, times(0)).vault(any(),any(),any(),anyBoolean(),any(),any());
+    }
+
+    @Test
+    public void testPullEmptyPaths() throws VaultException {
         var server = new Server("URL", null, false, null, null, "NAMESPACE", List.of(), false, 1);
         var vaultBackendProviderMock = Mockito.mock(VaultBackendProvider.class);
         var vaultMock = Mockito.mock(Vault.class);
@@ -71,7 +88,7 @@ public class TestVaults {
     }
 
     @Test
-    public void testPush() throws VaultException {
+    public void testPushEmptyPaths() throws VaultException {
         var server = new Server("URL", null, false, null, null, "NAMESPACE", List.of(), false, 1);
         var vaultBackendProviderMock = Mockito.mock(VaultBackendProvider.class);
         var vaultMock = Mockito.mock(Vault.class);
@@ -79,6 +96,49 @@ public class TestVaults {
         var vaultClient = Vaults.createForBackend(vaultBackendProviderMock);
 
         vaultClient.push(List.of(server), null);
+
+        verify(vaultBackendProviderMock, times(1)).vault(any(),any(),any(),anyBoolean(),any(),any());
+    }
+
+    @Test
+    public void testPull() throws VaultException {
+        List<Path> paths = randomPaths(10, 10);
+        var server = new Server("URL", null, false, null, null, "NAMESPACE", paths, false, 1);
+        var vaultBackendProviderMock = Mockito.mock(VaultBackendProvider.class);
+        var vaultMock = getVaultMock(paths);
+        when(vaultBackendProviderMock.vault(any(),any(),any(),anyBoolean(),any(),any())).thenReturn(vaultMock);
+        var vaultClient = Vaults.createForBackend(vaultBackendProviderMock);
+
+        vaultClient.pull(List.of(server), new Properties());
+
+        verify(vaultBackendProviderMock, times(1)).vault(any(),any(),any(),anyBoolean(),any(),any());
+    }
+
+    private static Vault getVaultMock(List<Path> paths) throws VaultException {
+        var vaultMock = Mockito.mock(Vault.class);
+        var logicalMock = Mockito.mock(Logical.class);
+        var logicalResponseMock = Mockito.mock(LogicalResponse.class);
+        when(logicalResponseMock.getData()).thenReturn(secretsFromPaths(paths));
+        when(logicalMock.read(any())).thenReturn(logicalResponseMock);
+        when(logicalMock.list(any())).thenReturn(logicalResponseMock);
+        when(logicalMock.write(any(),any())).thenReturn(logicalResponseMock);
+        when(vaultMock.logical()).thenReturn(logicalMock);
+        return vaultMock;
+    }
+
+    @Test
+    public void testPush() throws VaultException {
+        List<Path> paths = randomPaths(10, 10);
+        var server = new Server("URL", null, false, null, null, "NAMESPACE", paths, false, 1);
+        var vaultBackendProviderMock = Mockito.mock(VaultBackendProvider.class);
+        var propertyMap = propertiesFromPaths(paths);
+        var vaultMock = getVaultMock(paths);
+        when(vaultBackendProviderMock.vault(any(),any(),any(),anyBoolean(),any(),any())).thenReturn(vaultMock);
+        var vaultClient = Vaults.createForBackend(vaultBackendProviderMock);
+        var properties = new Properties();
+        properties.putAll(propertyMap);
+
+        vaultClient.push(List.of(server), properties);
 
         verify(vaultBackendProviderMock, times(1)).vault(any(),any(),any(),anyBoolean(),any(),any());
     }
--- src/test/java/com/homeofthewizard/maven/plugins/vault/VaultTestHelper.java
+++ src/test/java/com/homeofthewizard/maven/plugins/vault/VaultTestHelper.java
@@ -4,6 +4,8 @@ import com.homeofthewizard.maven.plugins.vault.config.Mapping;
 import com.homeofthewizard.maven.plugins.vault.config.Path;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -24,5 +26,19 @@ public class VaultTestHelper {
 
     static List<Path> randomPaths(int pathCount, int mappingCount) {
         return IntStream.range(0, pathCount).mapToObj(i -> randomPath(mappingCount)).collect(Collectors.toList());
+    }
+
+    static Map<String, String> secretsFromPaths(List<Path> paths){
+        return paths.stream()
+                .map(Path::getMappings)
+                .flatMap(List::stream)
+                .collect(Collectors.toMap(Mapping::getKey, Mapping::getKey));
+    }
+
+    static Map<String, String> propertiesFromPaths(List<Path> paths){
+        return paths.stream()
+                .map(Path::getMappings)
+                .flatMap(List::stream)
+                .collect(Collectors.toMap(Mapping::getProperty, Mapping::getKey));
     }
 }

6dff04e73ebb9e441b9daee19a367a0f6365504b
Ozgun OZ <ozgunn.oz@gmail.com>
2023-07-01 15:54:00 +0200

    fix vault 1.13.x github authentication

--- docker/vault/enable-github-auth.sh
+++ docker/vault/enable-github-auth.sh
@@ -1,5 +1,5 @@
 vault login 6edb70bd-3398-1f42-d67e-b9db377223cd
 vault auth enable github
-vault write auth/github/config organization=vault-plugin-test
+vault write auth/github/config organization=vault-plugin-test organization-id=0
 vault write auth/github/map/teams/test-team value=dev-policy
 vault policy write dev-policy /home/vault/dev-policy.hcl
